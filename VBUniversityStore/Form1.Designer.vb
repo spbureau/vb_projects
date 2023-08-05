54e4e7a5c1946684c1c43c58b9011f115e1e9cf5
Simon Peters <simonpetersonline@gmail.com>
2023-08-05 20:09:12 +0000

    wrote code for jenny's vb store app

--- VBUniversityStore/Form1.Designer.vb
+++ VBUniversityStore/Form1.Designer.vb
@@ -23,25 +23,25 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.totalDueTextBox = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.salesTaxTextBox = New System.Windows.Forms.TextBox()
+        Me.subTotalTextBox = New System.Windows.Forms.TextBox()
+        Me.quantityTextBox = New System.Windows.Forms.TextBox()
+        Me.priceTextBox = New System.Windows.Forms.TextBox()
+        Me.isbnTextBox = New System.Windows.Forms.TextBox()
+        Me.bookTitleTextBox = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.TotalsButton = New System.Windows.Forms.Button()
         Me.ComputeButton = New System.Windows.Forms.Button()
         Me.ResetButton = New System.Windows.Forms.Button()
-        Me.TotalsButton = New System.Windows.Forms.Button()
-        Me.ExitButton = New System.Windows.Forms.Button()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.Label6 = New System.Windows.Forms.Label()
-        Me.bookTitleTextBox = New System.Windows.Forms.TextBox()
-        Me.isbnTextBox = New System.Windows.Forms.TextBox()
-        Me.priceTextBox = New System.Windows.Forms.TextBox()
-        Me.quantityTextBox = New System.Windows.Forms.TextBox()
-        Me.subTotalTextBox = New System.Windows.Forms.TextBox()
-        Me.salesTaxTextBox = New System.Windows.Forms.TextBox()
-        Me.Label7 = New System.Windows.Forms.Label()
-        Me.Label8 = New System.Windows.Forms.Label()
-        Me.totalDueTextBox = New System.Windows.Forms.TextBox()
         Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -75,6 +75,158 @@ Partial Class Form1
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Textbook Sale Information"
         '
+        'totalDueTextBox
+        '
+        Me.totalDueTextBox.Location = New System.Drawing.Point(143, 308)
+        Me.totalDueTextBox.Name = "totalDueTextBox"
+        Me.totalDueTextBox.ReadOnly = True
+        Me.totalDueTextBox.Size = New System.Drawing.Size(149, 27)
+        Me.totalDueTextBox.TabIndex = 19
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.ForeColor = System.Drawing.Color.Black
+        Me.Label8.Location = New System.Drawing.Point(16, 309)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(78, 20)
+        Me.Label8.TabIndex = 18
+        Me.Label8.Text = "Total Due:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.ForeColor = System.Drawing.Color.Black
+        Me.Label7.Location = New System.Drawing.Point(15, 168)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(72, 20)
+        Me.Label7.TabIndex = 17
+        Me.Label7.Text = "Quantity:"
+        '
+        'salesTaxTextBox
+        '
+        Me.salesTaxTextBox.Location = New System.Drawing.Point(143, 264)
+        Me.salesTaxTextBox.Name = "salesTaxTextBox"
+        Me.salesTaxTextBox.ReadOnly = True
+        Me.salesTaxTextBox.Size = New System.Drawing.Size(149, 27)
+        Me.salesTaxTextBox.TabIndex = 16
+        '
+        'subTotalTextBox
+        '
+        Me.subTotalTextBox.Location = New System.Drawing.Point(143, 213)
+        Me.subTotalTextBox.Name = "subTotalTextBox"
+        Me.subTotalTextBox.ReadOnly = True
+        Me.subTotalTextBox.Size = New System.Drawing.Size(149, 27)
+        Me.subTotalTextBox.TabIndex = 15
+        '
+        'quantityTextBox
+        '
+        Me.quantityTextBox.Location = New System.Drawing.Point(143, 166)
+        Me.quantityTextBox.Name = "quantityTextBox"
+        Me.quantityTextBox.Size = New System.Drawing.Size(149, 27)
+        Me.quantityTextBox.TabIndex = 13
+        '
+        'priceTextBox
+        '
+        Me.priceTextBox.Location = New System.Drawing.Point(143, 123)
+        Me.priceTextBox.Name = "priceTextBox"
+        Me.priceTextBox.Size = New System.Drawing.Size(149, 27)
+        Me.priceTextBox.TabIndex = 12
+        '
+        'isbnTextBox
+        '
+        Me.isbnTextBox.Location = New System.Drawing.Point(143, 82)
+        Me.isbnTextBox.Name = "isbnTextBox"
+        Me.isbnTextBox.Size = New System.Drawing.Size(242, 27)
+        Me.isbnTextBox.TabIndex = 11
+        '
+        'bookTitleTextBox
+        '
+        Me.bookTitleTextBox.Location = New System.Drawing.Point(143, 45)
+        Me.bookTitleTextBox.Name = "bookTitleTextBox"
+        Me.bookTitleTextBox.Size = New System.Drawing.Size(242, 27)
+        Me.bookTitleTextBox.TabIndex = 10
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.ForeColor = System.Drawing.Color.Black
+        Me.Label6.Location = New System.Drawing.Point(16, 265)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(74, 20)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Sales Tax:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.ForeColor = System.Drawing.Color.Black
+        Me.Label5.Location = New System.Drawing.Point(16, 216)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(72, 20)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "SubTotal:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(16, 194)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 20)
+        Me.Label4.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.Color.Black
+        Me.Label3.Location = New System.Drawing.Point(16, 126)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 20)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Price:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.Black
+        Me.Label2.Location = New System.Drawing.Point(16, 84)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(131, 20)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "ISBN (Indentifier):"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(16, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(81, 20)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Book Title:"
+        '
+        'ExitButton
+        '
+        Me.ExitButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.ExitButton.ForeColor = System.Drawing.Color.Black
+        Me.ExitButton.Location = New System.Drawing.Point(512, 354)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(75, 33)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = False
+        '
+        'TotalsButton
+        '
+        Me.TotalsButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.TotalsButton.ForeColor = System.Drawing.Color.Black
+        Me.TotalsButton.Location = New System.Drawing.Point(397, 354)
+        Me.TotalsButton.Name = "TotalsButton"
+        Me.TotalsButton.Size = New System.Drawing.Size(75, 33)
+        Me.TotalsButton.TabIndex = 2
+        Me.TotalsButton.Text = "&Totals"
+        Me.TotalsButton.UseVisualStyleBackColor = False
+        '
         'ComputeButton
         '
         Me.ComputeButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
@@ -89,6 +241,7 @@ Partial Class Form1
         'ResetButton
         '
         Me.ResetButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.ResetButton.Enabled = False
         Me.ResetButton.ForeColor = System.Drawing.Color.Black
         Me.ResetButton.Location = New System.Drawing.Point(276, 354)
         Me.ResetButton.Name = "ResetButton"
@@ -96,158 +249,6 @@ Partial Class Form1
         Me.ResetButton.TabIndex = 0
         Me.ResetButton.Text = "&Reset"
         Me.ResetButton.UseVisualStyleBackColor = False
-        '
-        'TotalsButton
-        '
-        Me.TotalsButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.TotalsButton.ForeColor = System.Drawing.Color.Black
-        Me.TotalsButton.Location = New System.Drawing.Point(397, 354)
-        Me.TotalsButton.Name = "TotalsButton"
-        Me.TotalsButton.Size = New System.Drawing.Size(75, 33)
-        Me.TotalsButton.TabIndex = 2
-        Me.TotalsButton.Text = "&Totals"
-        Me.TotalsButton.UseVisualStyleBackColor = False
-        '
-        'ExitButton
-        '
-        Me.ExitButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.ExitButton.ForeColor = System.Drawing.Color.Black
-        Me.ExitButton.Location = New System.Drawing.Point(512, 354)
-        Me.ExitButton.Name = "ExitButton"
-        Me.ExitButton.Size = New System.Drawing.Size(75, 33)
-        Me.ExitButton.TabIndex = 3
-        Me.ExitButton.Text = "E&xit"
-        Me.ExitButton.UseVisualStyleBackColor = False
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.ForeColor = System.Drawing.Color.Black
-        Me.Label1.Location = New System.Drawing.Point(16, 49)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(81, 20)
-        Me.Label1.TabIndex = 4
-        Me.Label1.Text = "Book Title:"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.ForeColor = System.Drawing.Color.Black
-        Me.Label2.Location = New System.Drawing.Point(16, 84)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(131, 20)
-        Me.Label2.TabIndex = 5
-        Me.Label2.Text = "ISBN (Indentifier):"
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.ForeColor = System.Drawing.Color.Black
-        Me.Label3.Location = New System.Drawing.Point(16, 126)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(47, 20)
-        Me.Label3.TabIndex = 6
-        Me.Label3.Text = "Price:"
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(16, 194)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(0, 20)
-        Me.Label4.TabIndex = 7
-        '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.ForeColor = System.Drawing.Color.Black
-        Me.Label5.Location = New System.Drawing.Point(16, 216)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(72, 20)
-        Me.Label5.TabIndex = 8
-        Me.Label5.Text = "SubTotal:"
-        '
-        'Label6
-        '
-        Me.Label6.AutoSize = True
-        Me.Label6.ForeColor = System.Drawing.Color.Black
-        Me.Label6.Location = New System.Drawing.Point(16, 265)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(74, 20)
-        Me.Label6.TabIndex = 9
-        Me.Label6.Text = "Sales Tax:"
-        '
-        'bookTitleTextBox
-        '
-        Me.bookTitleTextBox.Location = New System.Drawing.Point(143, 45)
-        Me.bookTitleTextBox.Name = "bookTitleTextBox"
-        Me.bookTitleTextBox.Size = New System.Drawing.Size(242, 27)
-        Me.bookTitleTextBox.TabIndex = 10
-        '
-        'isbnTextBox
-        '
-        Me.isbnTextBox.Location = New System.Drawing.Point(143, 82)
-        Me.isbnTextBox.Name = "isbnTextBox"
-        Me.isbnTextBox.Size = New System.Drawing.Size(242, 27)
-        Me.isbnTextBox.TabIndex = 11
-        '
-        'priceTextBox
-        '
-        Me.priceTextBox.Location = New System.Drawing.Point(143, 123)
-        Me.priceTextBox.Name = "priceTextBox"
-        Me.priceTextBox.Size = New System.Drawing.Size(149, 27)
-        Me.priceTextBox.TabIndex = 12
-        '
-        'quantityTextBox
-        '
-        Me.quantityTextBox.Location = New System.Drawing.Point(143, 166)
-        Me.quantityTextBox.Name = "quantityTextBox"
-        Me.quantityTextBox.Size = New System.Drawing.Size(149, 27)
-        Me.quantityTextBox.TabIndex = 13
-        '
-        'subTotalTextBox
-        '
-        Me.subTotalTextBox.Location = New System.Drawing.Point(143, 213)
-        Me.subTotalTextBox.Name = "subTotalTextBox"
-        Me.subTotalTextBox.ReadOnly = True
-        Me.subTotalTextBox.Size = New System.Drawing.Size(149, 27)
-        Me.subTotalTextBox.TabIndex = 15
-        '
-        'salesTaxTextBox
-        '
-        Me.salesTaxTextBox.Location = New System.Drawing.Point(143, 264)
-        Me.salesTaxTextBox.Name = "salesTaxTextBox"
-        Me.salesTaxTextBox.ReadOnly = True
-        Me.salesTaxTextBox.Size = New System.Drawing.Size(149, 27)
-        Me.salesTaxTextBox.TabIndex = 16
-        '
-        'Label7
-        '
-        Me.Label7.AutoSize = True
-        Me.Label7.ForeColor = System.Drawing.Color.Black
-        Me.Label7.Location = New System.Drawing.Point(15, 168)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(72, 20)
-        Me.Label7.TabIndex = 17
-        Me.Label7.Text = "Quantity:"
-        '
-        'Label8
-        '
-        Me.Label8.AutoSize = True
-        Me.Label8.ForeColor = System.Drawing.Color.Black
-        Me.Label8.Location = New System.Drawing.Point(16, 309)
-        Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(78, 20)
-        Me.Label8.TabIndex = 18
-        Me.Label8.Text = "Total Due:"
-        '
-        'totalDueTextBox
-        '
-        Me.totalDueTextBox.Location = New System.Drawing.Point(143, 308)
-        Me.totalDueTextBox.Name = "totalDueTextBox"
-        Me.totalDueTextBox.ReadOnly = True
-        Me.totalDueTextBox.Size = New System.Drawing.Size(149, 27)
-        Me.totalDueTextBox.TabIndex = 19
         '
         'Form1
         '
--- VBUniversityStore/Form1.vb
+++ VBUniversityStore/Form1.vb
@@ -6,13 +6,59 @@
 
     Private Sub ComputeButton_Click(sender As Object, e As EventArgs) Handles ComputeButton.Click
         ' INPUT
+        ' Declare constants
+        Const SalesTaxRate As Decimal = 0.0725
+
         ' Declare variables
         Dim price As Decimal
         Dim quantity As Integer
+        Dim subtotal As Decimal
+        Dim salestax As Decimal
+        Dim TotalDue As Decimal
+
 
         ' Converting values from textbox controls to memory variables
-        ' price = 
+        price = Decimal.Parse(priceTextBox.Text, Globalization.NumberStyles.Currency)
+        quantity = Integer.Parse(quantityTextBox.Text, Globalization.NumberStyles.Number)
+
+        ' Process - Computing value
+        ' Subtotal = quantity times price
+        subtotal = quantity * price
+
+        ' sales tax = sales tax rate times the subtotal minus discount amount
+        salestax = SalesTaxRate * subtotal
+
+        'Total due is the subtotal minus discount amount plus sales tax 
+        TotalDue = subtotal + salestax
+
+        ' Output - To display computed values in their textboxes
+        subTotalTextBox.Text = subtotal.ToString("C")
+        salesTaxTextBox.Text = salestax.ToString("C")
+        totalDueTextBox.Text = TotalDue.ToString("C")
+
+        'Enable/Disable Button
+        'disabling compute button when you click compute
+        ComputeButton.Enabled = False
+
+        'enabling reset button when compute button is clicked
+        ResetButton.Enabled = True
 
 
+    End Sub
+
+    Private Sub ResetButton_Click(sender As Object, e As EventArgs) Handles ResetButton.Click
+        ResetButton.Enabled = False ' to disable reset button when clicked
+        'reset button to clear all the textboxes values
+
+        bookTitleTextBox.Clear()
+        isbnTextBox.Clear()
+        priceTextBox.Clear()
+        quantityTextBox.Clear()
+        subTotalTextBox.Clear()
+        salesTaxTextBox.Clear()
+        totalDueTextBox.Clear()
+
+        'enabling compute button back when reset button is clicked
+        ComputeButton.Enabled = True
     End Sub
 End Class
